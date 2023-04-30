d3d4cf5a059662e27a010849e3de458e7b3befdf
Iryna_Kobyzhcha <irinakobyzhcha16@ukr.net>
2023-04-30 23:29:47 +0300

    add galary-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,67 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const galleryList =document.querySelector('.gallery');
+const pictureCard = createGalleryPictureCard (galleryItems);
+
+galleryList.insertAdjacentHTML('beforeend', pictureCard);
+galleryList.addEventListener('click', onGalleryPictureCardClick);
+
+function createGalleryPictureCard (galleryItems){
+  return galleryItems
+  .map(({preview, original, description}) => {
+return`
+<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>
+    `
+  })
+  .join('');
+};
+
+function onGalleryPictureCardClick (evt){
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+	}
+	const imgUrl = evt.target.dataset.source;
+
+	return showImgModal(imgUrl);
+}
+
+const instance = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+})
+
+// function showImgModal (imgUrl) {
+  
+//   onShow: (instance) => {
+//     console.log('onShow', instance);
+
+//     document.addEventListener("keyup", checkEsc)};
+
+//   onClose: (instance) => {
+//     console.log('onClose', instance);
+
+//     document.removeEventListener("keyup", checkEsc);
+//   }
+// instance.show();
+
+// function checkEsc({ code }) {
+//   console.log(code);
+//   if (code === "Escape") {
+//     instance.close();
+//   }
+// }
+// }
