18379b15791df253a23d503206859ecd7176971c
Iryna_Kobyzhcha <irinakobyzhcha16@ukr.net>
2023-05-02 20:30:28 +0300

    add task 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import throttle from 'lodash.throttle';	
+import Player from '@vimeo/player';
+
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const VIDEO_TIME_STORAGE_KEY = 'videoplayer-current-time';
+
+const check = () => {
+	if (localStorage.getItem(VIDEO_TIME_STORAGE_KEY) !== null) {
+		return localStorage.getItem(VIDEO_TIME_STORAGE_KEY);
+	}
+	return 0;	
+}
+
+const currentTime = check();
+
+player.setCurrentTime(currentTime);	
+
+player.on('timeupdate', throttle(setCurrentTime, 1000));
+
+function setCurrentTime(value) {
+	localStorage.setItem(VIDEO_TIME_STORAGE_KEY, value.seconds);
+}
 
